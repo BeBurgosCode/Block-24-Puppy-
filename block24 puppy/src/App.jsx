19067ca4c42164cc9e408da7031307bbe86b5cb1
Bernice Burgos <berniceburgos22@gmail.com>
2024-01-04 01:06:54 -0500

    change 4

--- block24 puppy/src/App.jsx
+++ block24 puppy/src/App.jsx
@@ -3,18 +3,36 @@ import { puppyList } from "./data.js";
 import "./App.css";
 
 function App() {
-
   const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
 
   console.log("puppyList: ", puppyList);
 
   return (
     <div className="App">
-      {
-        puppies.map((puppy) => {
-             return <p key={puppy.id}>{puppy.name}</p>;
-           })
-       }
+      {puppies.map((puppy) => {
+        return (
+          <p
+            onClick={() => {
+              setFeatPupId(puppy.id);
+            }}
+            key={() => {
+              console.log("puppy.id: ", puppy.id);
+            }}
+          >
+            {puppy.name}
+            {featPupId && (
+              <div>
+                <h2>{featuredPup.name}</h2>
+                <ul>
+                  <li>Age: {featuredPup.age}</li>
+                  <li>Email: {featuredPup.email}</li>
+                </ul>
+              </div>
+            )}
+          </p>
+        );
+      })}
     </div>
   );
 }
